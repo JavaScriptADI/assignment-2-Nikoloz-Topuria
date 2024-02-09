6112aae028b2e4cfb87911537b5db01d932cefed
Nikoloz-Topuria <topuriaanikoloz@gmail.com>
2024-02-10 02:15:52 +0400

    add homework 2

---
+++ script.js
@@ -0,0 +1,78 @@
+//  n1 
+ 
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+  }
+  
+  let num1 = 5;
+  let num2 = 7;
+  
+  let sum = addTwoNumbers(num1, num2);
+  
+  console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
+
+
+
+//   n2 
+   
+  function multiplyThreeNumbers(number1, number2, num3ber) {
+    return number1 * number2 * number3;
+  }
+  
+  let number1 = 2;
+  let number2 = 3;
+  let number3 = 4;
+  
+  let multiplied = multiplyThreeNumbers(number1, number2, number3);
+  
+  console.log(`Multiplied of ${number1}, ${number2}, and ${number3} is: ${multiplied}`);
+
+
+
+// n3 
+
+function Hello(Nika) {
+  console.log('Hello ' + Nika);
+}
+Hello('Nika!'); 
+
+
+
+
+
+
+
+// homework 2 
+
+console.log(true);
+console.log(false);
+
+console.log(`2 === "2" = ${2 === "2"}`);
+
+console.log(`2 === 2 = ${2 === 2}`);
+
+console.log(`10 % 3 === 0 = ${10 % 3 === 0}`);
+
+console.log(`10 % 3 === 1 = ${10 % 3 === 1}`);
+
+console.log(`true && false = ${true && false}`);
+
+console.log(`false || true = ${false || true}`);
+
+console.log(`true || false = ${true || false}`);
+
+console.log(`5 + 8 >= 12 || 19 < 90 || !false = ${5 + 8 >= 12 || 19 < 90 || !false}`);
+
+console.log(`"Josh" == "josh" && !true && false && 17.9 + 0.1 == 18.1 = ${"Josh" == "josh" && !true && false && 17.9 + 0.1 == 18.1}`);
+
+console.log(`0.1 + 0.2 == 0.3 = ${0.1 + 0.2 == 0.3}`);
+
+console.log(`Math.min(0, 1, 2, 10, 11, 100) && Math.max(-100, -90, -1, -0.9, 0) = ${Math.min(0, 1, 2, 10, 11, 100) && Math.max(-100, -90, -1, -0.9, 0)}`);
+
+console.log(`Math.max(0, 1, 2, 10, 11, 100) && Math.min(-100, -90, -1, -0.9, 0) = ${Math.max(0, 1, 2, 10, 11, 100) && Math.min(-100, -90, -1, -0.9, 0)}`);
+
+console.log(`Math.min(0, 1, 2, 10, 11, 100) || Math.min(-100, -90, -1, -0.9, 0) = ${Math.min(0, 1, 2, 10, 11, 100) || Math.min(-100, -90, -1, -0.9, 0)}`);
+
+console.log(`!Math.min(0, 1, 2, 10, 11, 100) || !Math.max(-100, -90, -1, -0.9, 0) = ${!Math.min(0, 1, 2, 10, 11, 100) || !Math.max(-100, -90, -1, -0.9, 0)}`);
+
+// ჩელენჯი რაზე იყო ვეღარ გავიხსენე:{{
